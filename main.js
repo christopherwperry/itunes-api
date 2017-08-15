6b28e8f3c02fa8d0bddd7ed8a2c0c3f23948b8cb
christopherwperry <christopherwperry@gmail.com>
2017-08-15 14:30:45 -0400

    change background picture, more styling, and comments in js

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
-
+//Establish variables to pull key elements from HTML to add function to
 let search = document.querySelector('.input');
 let button = document.querySelector('.button');
 let results = document.querySelector('.results');
 let audio = document.querySelector('.music-player');
 let playing = document.querySelector('.now-playing');
 
+//Event listener responds to click on submit button and performs fetch of 
 button.addEventListener("click", function(){
   let search_item = search.value;
   let url = 'https://itunes.apple.com/search?term=';
