7cdf8a4ee96bcb3988626addee1e6d51eba68f65
christopherwperry <christopherwperry@gmail.com>
2017-08-20 21:36:03 -0400

    project complete

--- main.js
+++ main.js
@@ -50,41 +50,41 @@ button.addEventListener("click", function(){
       });
 
 
-function formSubmit() {
-  let search_item = search.value;
-  let url = 'https://itunes.apple.com/search?term=';
-  let limit = '&limit=15'
-  let fullurl = url + search_item + limit;
-  console.log(fullurl);
-  fetch(fullurl)
-      .then(function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          response.json().then(function(data) {
-            console.log(data);
-            results.innerHTML = "";
-            for(let i = 0; i < data.results.length; i++){
-              let artwork = data.results[i].artworkUrl100;
-              let track = data.results[i].trackName;
-              let artist = data.results[i].artistName;
-              let sample = data.results[i].previewUrl;
-              let new_box = document.createElement("div");
-              new_box.setAttribute("class", "result");
-              new_box.addEventListener("click", function(){
-                audio.src = sample;
-                playing.innerHTML = "Now Playing: " + artist + " - " + track;
-              });
-              let result = `
-              <img class="album-cover" src="${artwork}">
-              <p class="song-title">${track}</p>
-              <p class="band-name">${artist}</p>
-              `;
-              new_box.innerHTML = result;
-              results.appendChild(new_box);
-            };
-          })
-        })
-}
+// function formSubmit() {
+//   let search_item = search.value;
+//   let url = 'https://itunes.apple.com/search?term=';
+//   let limit = '&limit=15'
+//   let fullurl = url + search_item + limit;
+//   console.log(fullurl);
+//   fetch(fullurl)
+//       .then(function(response) {
+//           if (response.status !== 200) {
+//             console.log('Looks like there was a problem. Status Code: ' +
+//               response.status);
+//             return;
+//           }
+//           response.json().then(function(data) {
+//             console.log(data);
+//             results.innerHTML = "";
+//             for(let i = 0; i < data.results.length; i++){
+//               let artwork = data.results[i].artworkUrl100;
+//               let track = data.results[i].trackName;
+//               let artist = data.results[i].artistName;
+//               let sample = data.results[i].previewUrl;
+//               let new_box = document.createElement("div");
+//               new_box.setAttribute("class", "result");
+//               new_box.addEventListener("click", function(){
+//                 audio.src = sample;
+//                 playing.innerHTML = "Now Playing: " + artist + " - " + track;
+//               });
+//               let result = `
+//               <img class="album-cover" src="${artwork}">
+//               <p class="song-title">${track}</p>
+//               <p class="band-name">${artist}</p>
+//               `;
+//               new_box.innerHTML = result;
+//               results.appendChild(new_box);
+//             };
+//           })
+//         })
+// };
